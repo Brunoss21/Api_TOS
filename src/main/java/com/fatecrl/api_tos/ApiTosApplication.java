dfb605a40a3aba21a5ea8abab5a82840aaa36baa
Bruno da Silva Santos <bruno_silva1420@hotmail.com>
2024-10-17 15:58:55 -0300

    Inicializando Projeto

---
+++ src/main/java/com/fatecrl/api_tos/ApiTosApplication.java
@@ -0,0 +1,13 @@
+package com.fatecrl.api_tos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fatecrl/api_tos/ApiTosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatecrl.api_tos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
